19042b745e2ae71ef62cc369477a42f1485728a7
rebotak <rebotak@gmail.com>
2021-08-14 22:29:52 +0700

    update axios, based on dependabots

--- src/store/articles.js
+++ src/store/articles.js
@@ -5,44 +5,42 @@ export const namespaced = true
 export const state = {
   data: [],
   pending: false,
-  error: null
+  error: null,
 }
 
 export const events = {
   RETRIEVE: 'RETRIEVE',
   RETRIEVE_SUCCEEDED: 'RETRIEVE_SUCCEEDED',
-  RETRIEVE_FAILED: 'RETRIEVE_FAILED'
-}
-
-export const mutations = {
-  [events.RETRIEVE] (state) {
-    state.pending = true
-    state.error = null
-  },
-  [events.RETRIEVE_SUCCEEDED] (state, payload) {
-    state.data = payload.data.results
-    state.pending = false
-    state.error = null
-  },
-  [events.RETRIEVE_FAILED] (state, payload) {
-    state.pending = false
-    state.error = payload
-    state.data = []
-  }
+  RETRIEVE_FAILED: 'RETRIEVE_FAILED',
 }
 
 export const actions = {
-  [events.RETRIEVE] (ctx) {
+  [events.RETRIEVE](ctx) {
     const url = 'https://opentdb.com/api.php?amount=10'
     const request = axios.get(url)
     ctx.commit(events.RETRIEVE, request)
   },
+}
 
-
+export const mutations = {
+  [events.RETRIEVE](state) {
+    state.pending = true
+    state.error = null
+  },
+  [events.RETRIEVE_SUCCEEDED](state, payload) {
+    state.data = payload.data.results
+    state.pending = false
+    state.error = null
+  },
+  [events.RETRIEVE_FAILED](state, payload) {
+    state.pending = false
+    state.error = payload
+    state.data = []
+  },
 }
 
 export const getters = {
-  data: state => state.data,
-  pending: state => state.pending,
-  error: state => state.error
+  data: (state) => state.data,
+  pending: (state) => state.pending,
+  error: (state) => state.error,
 }
